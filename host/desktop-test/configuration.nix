69507835b2a0beddf2d61e9bbe0ea28d343b5aba
Robert Andersson <kemichal@gmail.com>
2024-08-11 15:24:19 +0200

    Fix locale

--- host/desktop-test/configuration.nix
+++ host/desktop-test/configuration.nix
@@ -17,6 +17,18 @@
   networking.hostName = "desktop-test";
   time.timeZone = "Europe/Stockholm";
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sv_SE.UTF-8";
+    LC_IDENTIFICATION = "sv_SE.UTF-8";
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_MONETARY = "sv_SE.UTF-8";
+    LC_NAME = "sv_SE.UTF-8";
+    LC_NUMERIC = "sv_SE.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_TELEPHONE = "sv_SE.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
+  };
+
   services.xserver.xkb.layout = "se";
   console.keyMap = "sv-latin1";
 
