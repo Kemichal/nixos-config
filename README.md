09f42b5ac979983ac65e48acbab12238b1713195
Robert Andersson <kemichal@gmail.com>
2024-08-11 14:59:00 +0200

    Fix repo urls

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ nix flake update ~/nixos-config
 ```
 sudo nixos-rebuild switch --flake ~/nixos-config
 ```
+
+Clean up
+```
+sudo nix-collect-garbage
+```
--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "NixOS configuration";
   inputs = {
-    nixpkg.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     disko = {
       url = "github:nix-community/disko";
@@ -9,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
