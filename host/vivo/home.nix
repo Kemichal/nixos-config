cfaa5c0e026b52ef6161db60d212c6949b7609e3
Robert Andersson <kemichal@gmail.com>
2024-08-21 14:59:26 +0200

    Add rofi config to home-manager

--- host/vivo/home.nix
+++ host/vivo/home.nix
@@ -30,6 +30,11 @@
     src = ./files/hyprland.conf;
     pam_kwallet_init = "${pkgs.kdePackages.kwallet-pam}/libexec/pam_kwallet_init";
   };
+  xdg.configFile."rofi/config.rasi".source = pkgs.substituteAll {
+    src = ./files/rofi/config.rasi;
+    kittyPath = "${pkgs.kitty}/bin/kitty";
+    themesPath = "${pkgs.rofi-wayland}/share/rofi/themes";
+  };
 
   programs.firefox = {
     enable = true;
